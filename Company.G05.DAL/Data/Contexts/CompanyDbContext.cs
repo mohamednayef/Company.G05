9a8cae84807ab1c32965c0a5a3c117665164c86f
mohamednayef <mohamednayef2002@gmail.com>
2025-09-26 03:23:15 +0300

    Implementation of DAL

---
+++ Company.G05.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using Company.G05.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.G05.DAL.Data.Contexts;
+
+public class CompanyDbContext : DbContext
+{
+    public CompanyDbContext() : base()
+    {
+        
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // base.OnConfiguring(optionsBuilder);
+        string connectionString = "Server=localhost,1433;Database=CompanyG05;User Id=sa;Password=StrongP@ssw0rd;Encrypt=False;TrustServerCertificate=True;";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+    public DbSet<Department> Departments { get; set; }
+}
---
+++ Company.G05.DAL/Models/Department.cs
@@ -0,0 +1,9 @@
+namespace Company.G05.DAL.Models;
+
+public class Department
+{
+    public int Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
