53091d762bb3d11b650ab2a368dc4f331d5677bb
mohamednayef <mohamednayef2002@gmail.com>
2025-09-27 15:38:10 +0300

    Implementation of Employee DAL

---
+++ Company.G05.DAL/Models/Employee.cs
@@ -0,0 +1,16 @@
+namespace Company.G05.DAL.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int? Age { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public decimal Salary { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime HirignDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
