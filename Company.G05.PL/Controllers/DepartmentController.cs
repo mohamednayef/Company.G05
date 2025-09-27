d3f52808e407930b45118c69eff70187e311260e
mohamednayef <mohamednayef2002@gmail.com>
2025-09-27 14:29:09 +0300

    Refactor department controller

--- Company.G05.PL/Controllers/DepartmentController.cs
+++ Company.G05.PL/Controllers/DepartmentController.cs
@@ -45,31 +45,57 @@ public class DepartmentController : Controller
         return View(model);
     }
 
-    public IActionResult Details(int id)
+    public IActionResult Details(int? id, string viewName = "Details")
     {
-        var department = _departmentRepository.Get(id);
-        return View(department);
+        if (id is null) return BadRequest();
+        
+        var department = _departmentRepository.Get(id.Value);
+        if (department is null) return NotFound( new { StatusCode = "404", Message = $"Department with id: {id.Value} not found" });
+        
+        return View(viewName, department);
     }
 
-    public IActionResult Edit(int id)
+    public IActionResult Edit(int? id)
     {
-        var department = _departmentRepository.Get(id);
-        return View(department);
+        // if (id is null) return BadRequest();
+        //
+        // var department = _departmentRepository.Get(id.Value);
+        // if (department is null) return NotFound( new { StatusCode = "404", Message = $"Department with id: {id.Value} not found" });
+        
+        // return View(department);
+        
+        return Details(id, viewName: "Edit");
     }
 
     [HttpPost]
-    public IActionResult Update(Department model)
+    // [ValidateAntiForgeryToken]
+    public IActionResult Edit([FromRoute]int id, Department model)
     {
         if (ModelState.IsValid)
         {
-            _departmentRepository.Update(model);
-            return RedirectToAction("Index");
+            if (id != model.Id) return BadRequest();
+            int count = _departmentRepository.Update(model);
+            if (count > 0)
+            {
+                return RedirectToAction("Index");
+            }
         }
         return View(model);
-        // _departmentRepository.Departments.update(model);
-        // return RedirectToAction("Index");
     }
-
+    
+    public IActionResult Delete(int? id)
+    {
+        // if (id is null) return BadRequest();
+        //
+        // var department = _departmentRepository.Get(id.Value);
+        // if (department is null) return NotFound( new { StatusCode = "404", Message = $"Department with id: {id.Value} not found" });
+        //
+        // return View(department);
+        
+        return Details(id, viewName: "Delete");
+    }
+    
+    [HttpPost]
     public IActionResult Delete(int id)
     {
         var department = _departmentRepository.Get(id);
